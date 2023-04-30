4ab090b9a13d49fd5e41a247f470a2fbdd4c2df0
kai <3470837+TheLeanMachine@users.noreply.github.com>
2023-04-30 18:17:46 +0200

    Initial import

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Home() {
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <h1>Advice Generator</h1>
+
+    </main>
+  )
+}
+
