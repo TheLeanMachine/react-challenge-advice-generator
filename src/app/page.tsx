3ea05c2025fb2f3df254daef2db212744251ee75
kai <3470837+TheLeanMachine@users.noreply.github.com>
2023-05-02 10:55:30 +0200

    Basic HTML and CSS skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import Image from 'next/image';
 
 export default function Home() {
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  // flex min-h-screen flex-col items-center justify-between p-24 bg-[#1f2631]
 
-      <h1>Advice Generator</h1>
+  return (
+    <main style={ {width: '400px', padding: '0 0 50px 0'} } className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 border-0 border-black border-solid">
+
+      <div className="rounded-lg bg-[#313a49] text-center border-0 border-white border-solid">
+        <h1 className="uppercase text-teal-300 py-5">Advice #123</h1>
+
+        <div className="p-5 text-white font-bold">
+          &quot;A quote from a wise man that is really, really, really, really, long, so it enforces a line-break - Kai&quot;
+        </div>
+
+        <div style={ {padding: '20px 0 60px 0'} } className="border-0 border-white border-solid">
+          <Image
+            src="/images/pattern-divider-desktop.svg"
+            alt="---"
+            width={444}
+            height={16}
+          />
+        </div>
+
+        <div className="absolute bottom-2 left-1/2 -translate-x-1/2 w-26 p-6 rounded-full bg-[#53ffac]">
+          <Image
+            src="/images/icon-dice.svg"
+            alt="---"
+            width={24}
+            height={24}
+            className="m-auto"
+          />
+        </div>
+      </div>
 
     </main>
   )
