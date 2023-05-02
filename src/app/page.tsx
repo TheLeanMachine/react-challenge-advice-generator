a2f5b29ca212f731bafcce2ce9195889a57b6670
kai <3470837+TheLeanMachine@users.noreply.github.com>
2023-05-02 14:31:43 +0200

    Inherit from React.Component base class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import React from 'react';
 import Image from 'next/image';
 
-import Advice from '../components/Advice'
+import { Advice } from '../components/Advice'
 
 export default function Home() {
 
---
+++ src/components/Advice.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+type AdviceProps = {
+    adviceId: string,
+    message: string
+};
+
+export class Advice extends Component<AdviceProps> {
+    render(): any {
+        return (
+            <>
+                <h1 className="uppercase text-teal-300 py-5">Advice #{this.props.adviceId}</h1>
+                <div className="p-5 text-white font-bold">
+                    &quot;{this.props.message}&quot;
+                </div>
+            </>
+        )
+    }
+}
