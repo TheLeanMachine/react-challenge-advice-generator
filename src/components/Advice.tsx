9cde1d330cb4b559a750b611d3f5166532935568
kai <3470837+TheLeanMachine@users.noreply.github.com>
2023-05-03 10:06:57 +0200

    Add AdviceState

--- src/components/Advice.tsx
+++ src/components/Advice.tsx
@@ -5,13 +5,27 @@ type AdviceProps = {
     message: string
 };
 
-export class Advice extends Component<AdviceProps> {
+type AdviceState = {
+    adviceId: string,
+    message: string
+};
+
+export class Advice extends Component<AdviceProps, AdviceState> {
+    constructor(props: AdviceProps) {
+        super(props);
+    }
+
+    // Before the component mounts, we initialise our state
+    componentWillMount() {
+        this.setState(this.props);
+    }
+
     render(): any {
         return (
             <>
-                <h1 className="uppercase text-teal-300 py-5">Advice #{this.props.adviceId}</h1>
+                <h1 className="uppercase text-teal-300 py-5">Advice #{this.state.adviceId}</h1>
                 <div className="p-5 text-white font-bold">
-                    &quot;{this.props.message}&quot;
+                    &quot;{this.state.message}&quot;
                 </div>
             </>
         )
