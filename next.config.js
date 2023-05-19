110964085cfabbbcbae06fe87e464d97c5831abe
kai <3470837+TheLeanMachine@users.noreply.github.com>
2023-05-19 17:20:07 +0200

    Hopefully fix deployment error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
+  experimental: {    
     appDir: true,
+    images: {
+      unoptimized: true,
+    },    
   },
 }
 
